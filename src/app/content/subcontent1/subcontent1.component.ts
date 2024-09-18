98dcb2e9a099a187fe230609fd02a028a77ec0b4
OksanaDmitrieva <oxana_dmitrieva@bk.ru>
2024-09-19 03:35:24 +0400

    становлена прокрутка и фон для блока кода

--- src/app/content/subcontent1/subcontent1.component.ts
+++ src/app/content/subcontent1/subcontent1.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Prism from 'prismjs';
 
 @Component({
   selector: 'app-subcontent1',  // Селектор компонента
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   templateUrl: './subcontent1.component.html',
   styleUrls: ['./subcontent1.component.css']
 })
-export class Subcontent1Component {
+export class Subcontent1Component implements AfterViewInit{
+  ngAfterViewInit() {
+    Prism.highlightAll();  // Инициализируем Prism.js для подсветки кода
+  }
 }
